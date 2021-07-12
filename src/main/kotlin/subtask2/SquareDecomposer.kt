4879c377f9cafef2c1330e6a23c9067cc5a84b32
olgarkovich <olgagarkovich@gmail.com>
2021-07-12 19:46:28 +0300

    complete tasks

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,40 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return if (number < 5) {
+            null
+        } else {
+            decompose(number.square(), number - 1)?.toTypedArray()
+        }
     }
+
+    private fun Int.square(): Int = (this * this)
+    private fun Int.diff(b: Int): Int = (this - b.square())
+
+    private fun decompose(number: Int, current: Int): ArrayList<Int>? {
+        for (i in current downTo 1) {
+            val diff = number.diff(i)
+            if (diff == 0) {
+                return arrayListOf(i)
+            } else if (diff < 0) {
+                return null
+            } else {
+                var sqrtDiff = sqrt(diff.toDouble()).toInt()
+                if (sqrtDiff >= i) {
+                    sqrtDiff = i - 1
+                }
+                val result = decompose(diff, sqrtDiff)
+                if (result != null) {
+                    result.add(i)
+                    return result
+                }
+            }
+        }
+        return null
+    }
+
 }
--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -2,8 +2,31 @@ package subtask3
 
 class TelephoneFinder {
 
-    // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        if (number.toLong() < 0) {
+            return null
+        }
+        val neighbors = mapOf(
+            '1' to arrayListOf('2', '4'),
+            '2' to arrayListOf('1', '3', '5'),
+            '3' to arrayListOf('2', '6'),
+            '4' to arrayListOf('1', '5', '7'),
+            '5' to arrayListOf('2', '4', '6', '8'),
+            '6' to arrayListOf('3', '5', '9'),
+            '7' to arrayListOf('4', '8'),
+            '8' to arrayListOf('0', '5', '7', '9'),
+            '9' to arrayListOf('6', '8'),
+            '0' to arrayListOf('8')
+        )
+
+        val result = mutableSetOf<String>()
+        number.forEachIndexed { i, num ->
+
+            neighbors[num]?.forEach {
+                result.add(number.substring(0, i) + it + number.substring(i + 1))
+            }
+        }
+
+        return result.toTypedArray()
     }
 }
