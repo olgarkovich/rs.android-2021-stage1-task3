ac0ebacaf7c0d70433d59c347f5bde54d9c93906
olgarkovich <olgagarkovich@gmail.com>
2021-07-12 18:38:07 +0300

    complete combinator

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -1,9 +1,27 @@
 package subtask1
 
+import kotlin.math.sqrt
+
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val m = array[0]
+        val n = array[1]
+
+        for (i in 1..(m / sqrt(2.0)).toInt()) {
+            val current = returnM(i, n)
+
+            if (current.toInt() == m) {
+                return i
+            }
+        }
+
+        return null
     }
+
+    private fun returnM(x: Int, n: Int): Long {
+        return factorial(n) / factorial(x) / factorial(n - x)
+    }
+
+    private fun factorial(n: Int): Long = if (n < 2) 1 else n * factorial(n - 1)
 }
